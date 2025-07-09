cdbdfa960aaf395bb0034d84a0d48ce1bfb0ce11
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2025-07-09 15:20:54 +0530

    fixed reference error

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI);
+
 const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI);
 const bcrypt = require("bcrypt");
 const express = require("express");
 const cors = require("cors");
